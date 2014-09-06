186ebe2e9b6ddf97964ba0448509e83cb91d6a5e
Mathias Vagni <matt@thousandfive.com>
2014-09-06 13:03:01 +0100

    Not more disk io

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var hoganExpress = require('hogan-express');
 var morgan = require('morgan');
-var crypto = require('crypto');
 var packageJSON = require('./package.json');
 var childProcess = require('child_process');
 var url = require('url');
@@ -11,6 +10,36 @@ var http = require('http');
 
 var server;
 
+/*
+Yo, the app.
+*/
+var app = express();
+
+
+/*
+The maximum amount of time we are going to give phantom to
+render an image.
+*/
+var PHANTOM_MAX_TIMEOUT = 1000;
+
+
+/*
+Stores a list of phantom processes & the port they run on
+e.g [{ process: , port: }, ...]
+*/
+var PHANTOM_PROCESSES = [];
+
+
+/*
+A list of available templates.
+*/
+var TEMPLATES = fs.readdirSync(__dirname + '/templates/images/');
+
+
+/*
+Setup our options
+Use --help when invoking this for... you... know... help.
+*/
 var options = require("nomnom")
     .option('app_port', {
       help: 'The port on which to run the main app.',
@@ -18,7 +47,7 @@ var options = require("nomnom")
     })
     .option('phantom_ports', {
         help: 'A list of ports on which you\'d like to start a phantom child process. An instance of phantomjs will be started for each port and reqeusts will be distributed accross them. Each phantomjs can deal with 10 concurrent requests.',
-        default : [6666],
+        default : [6001, 6002],
         list : true,
         position: 0
     })
@@ -28,91 +57,83 @@ var options = require("nomnom")
     })
     .parse();
 
-/*
-Yo, the app.
-*/
-var app = express();
 
 /*
-The maximum amount of time we are going to give phantom to
-render an image.
+Given the url of an image such as: /image/200/200/foo/?bar=yolo, this returns the
+equivalent path for the template, i.e: /html/foo/?bar=yolo
+
+@param {string} url - The request url you would like to transform
+@param {string} templateName - The name of the template.
 */
-var PHANTOM_MAX_TIMEOUT = 1000;
-
-/*
-Stores a list of phantom processes & the port they run on
-e.g [{ process: , port: }, ...]
-*/
-var PHANTOM_PROCESSES = [];
-
-/*
-A list of available templates.
-*/
-var TEMPLATES = fs.readdirSync(__dirname + '/templates/images/');
-
-
-/*
-Return a unique image url based on the version of renderer, template & data
-e.g '/0.0.0/header-area/7c20f3372ef740c4eb08ca56f44335a5'
-*/
-function makeImageUrl(width, height, templateName, data) {
-    var dataString = '';
-
-    for (var property in data) {
-        if (data.hasOwnProperty(property)) {
-            dataString += encodeURIComponent(property);
-            dataString += encodeURIComponent(data[property]);
-        }
-    }
-
-    dataString = crypto.createHash('md5').update(dataString).digest('hex');
-
-    return [
-        '/image-store',
-        packageJSON.version,
-        width + 'x' + height,
-        templateName,
-        dataString
-    ].join('/') + '.jpeg'
+function makeTemplateUrlFromImageUrl(reqUrl, templateName) {
+    var queryString = url.parse(reqUrl).search;
+    return 'http://localhost:' + options.app_port + '/html/' + templateName + '/' + queryString;
 }
 
+
 /*
-Given the url of an image such as: /image/foo/?bar=yolo, this returns the
-equivalent path for the template, i.e: /image/foo/?bar=yolo
+Send an image
+@param  {object} res - An express response object.
+@param {string} imageData - The imagedata (png)
 */
-function makeTemplateUrlFromImageUrl(reqUrl, templateName, templateData) {
-    var search = url.parse(reqUrl, true).search;
-    return 'http://localhost:' + options.app_port + '/html/' + templateName + '/' + search;
+function sendImage(res, imageData){
+    res.set('Content-Type', 'image/png');
+    res.send(new Buffer(imageData, 'base64'));
 }
+
+
+/*
+Returns next available phantomjs port.
+*/
+function getNextPhantomPort() {
+    // Distribute requests accross the available
+    // phantom processes.
+    PHANTOM_PROCESSES.push(PHANTOM_PROCESSES.shift());
+    return PHANTOM_PROCESSES[0].port;
+};
+
 
 /*
 Take a screenshot by posting to the next available phantomJS server.
+
+@param {number} options.width - The width of the screenshot you want
+@param {number} options.height - The height of the screenshot you want
+@param {string} options.url - The url you want to screenshot
+@param {function} callback - Callback (called with the raw image data)
 */
 function takeScreenshot(options, callback) {
+
+    var phantomRequest;
+    var imageData = '';
 
     var postData = JSON.stringify({
         width : options.width,
         height : options.height,
         url : options.url,
-        renderedImagePath : options.renderedImagePath,
         maxTimeout : PHANTOM_MAX_TIMEOUT
     });
 
-    var phantomRequest = http.request({
-        port: PHANTOM_PROCESSES[0].port,
+    function handleResponse(response) {
+        // When receiving data from phantom, keep it.
+        response.on('data', function(chunk){
+            imageData += chunk;
+        });
+
+        // When it's over, call back with the image data.
+        response.on('end', function(){
+            callback(imageData);
+        });
+    }
+
+    phantomRequest = http.request({
+        port: getNextPhantomPort(),
         path: '/',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Content-Length': postData.length
         }
-    }, function(){
-        callback();
-    });
-
-    // Distribute requests accross the available
-    // phantom processes.
-    PHANTOM_PROCESSES.push(PHANTOM_PROCESSES.shift());
+    }, handleResponse);
 
     // Send post data.
     phantomRequest.write(postData + '\n');
@@ -122,13 +143,17 @@ function takeScreenshot(options, callback) {
 
 /*
 Spawn X child processes of Phantom
+
+@param {number} number - The number of phantom processes you would like to start.
 */
 function spawnPhantoms(number) {
+
+    var phantom;
 
     for (var i = 0; i < number; i++) {
 
         var port = options.phantom_ports[i];
-        var phantom = childProcess.spawn('phantomjs', [
+        phantom = childProcess.spawn('phantomjs', [
             __dirname + '/phantomjs.js',
             port
         ]);
@@ -148,6 +173,7 @@ function spawnPhantoms(number) {
     }
 };
 
+
 /*
 Setup
 */
@@ -155,12 +181,13 @@ app.set('views', __dirname + '/templates/');
 app.set('layout', __dirname + '/templates/_layouts/image-base.html');
 app.engine('html', hoganExpress);
 
+
 /*
 Middleware
 */
 app.use('/', morgan('tiny'));
 app.use('/static/', express.static(__dirname + '/static/'));
-app.use('/image-store/', express.static(__dirname + '/image-store/'));
+
 
 /*
 Route - Templates index
@@ -171,6 +198,7 @@ app.get('/', function(req, res) {
         layout: '_layouts/app-base.html'
     });
 });
+
 
 /*
 Route - Rendere a template.
@@ -186,47 +214,29 @@ app.get('/html/:template/', function(req, res) {
     res.render('images/' + template + '/index.html', data);
 });
 
+
 /*
 Route - Render a template using phantomjs & respond as an image.
 */
 app.get('/image/:width/:height/:template/', function(req, res){
 
-    var width = req.params.width;
-    var height = req.params.height;
-
-    var templateData = req.query;
-    var templateName = req.params.template;
-    var templateUrl = makeTemplateUrlFromImageUrl(req.url, templateName, templateData);
-
-    var renderedImageUrl = makeImageUrl(width, height, templateName, templateData);
-    var renderedImagePath = __dirname + renderedImageUrl;
-
     var screenshotOptions = {
-        url : templateUrl,
-        width : width,
-        height : height,
-        renderedImagePath : renderedImagePath
+        url : makeTemplateUrlFromImageUrl(req.url, req.params.template),
+        width : req.params.width,
+        height : req.params.height
     };
 
-    // Redirect the user to the image.
-    function redirectToImage() {
-        res.redirect(renderedImageUrl);
-        res.end();
-    }
-
-    // Check if the file allready exists. If it does then just redirect to it.
-    fs.exists(renderedImagePath, function (exists) {
-        if (exists && options.cache) {
-            return redirectToImage();
-        }
-        takeScreenshot(screenshotOptions, redirectToImage);
+    takeScreenshot(screenshotOptions, function(imageData){
+        sendImage(res, imageData);
     });
 });
 
+
 /*
-Let's get this show on the road
+Start a bunch of phantom processes
 */
 spawnPhantoms(options.phantom_ports.length);
+
 
 /*
 Start the server
